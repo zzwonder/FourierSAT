c3635a6d5d3b0fba55450af0bb8fb36fddd4f3d8
Zhang <zz59@login3.nots.rice.edu>
2024-10-31 19:28:36 -0500

    update newexp.sh

--- newexp.sh
+++ newexp.sh
@@ -4,11 +4,8 @@ mkdir results_2024
 ./run.slurm randomInstances/CNF_1000 fouriersat unconstrained_SQUARE_HJ 1 square HJPROX_PARALLEL 0 0 dev 
 ./run.slurm randomInstances/XOR_1000 fouriersat unconstrained_SQUARE_HJ 1 square HJPROX_PARALLEL 0 0 dev
 ./run.slurm randomInstances/cnfxor fouriersat unconstrained_SQUARE_HJ 1 square HJPROX_PARALLEL 0 0 solver
-./run.slurm randomInstances/cards fouriersat unconstrained_SQUARE_HJ_0.8 1 square HJPROX_PARALLEL 0 0.8 solver
 
 ./run.slurm randomInstances/CNF_1000 fouriersat unconstrained_ABS_HJ 1 abs HJPROX_PARALLEL 0 0 dev 
 ./run.slurm randomInstances/XOR_1000 fouriersat unconstrained_ABS_HJ 1 abs HJPROX_PARALLEL 0 0 dev
 ./run.slurm randomInstances/cnfxor fouriersat unconstrained_ABS_HJ 1 abs HJPROX_PARALLEL 0 0 solver
-./run.slurm randomInstances/cards fouriersat unconstrained_ABS_HJ_0.8 1 abs HJPROX_PARALLEL 0 0.8 solver
-
 
